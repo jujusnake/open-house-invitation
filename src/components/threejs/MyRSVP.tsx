9ed735a2c524bd35eba20dd0b081b112281c4b3c
jujusnake <southpoley@gmail.com>
2023-11-13 08:49:10 +0900

    Feat: RSVP 완성

--- src/components/threejs/MyRSVP.tsx
+++ src/components/threejs/MyRSVP.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { Float, Mask, useGLTF, useMask } from "@react-three/drei";
+import { Float, Html, Mask, useGLTF, useMask } from "@react-three/drei";
 import * as THREE from "three";
 import { useFrame, useLoader, useThree } from "@react-three/fiber";
 
@@ -36,21 +36,23 @@ const MyRSVP = ({ selected, onSelect }: Props) => {
 
   return (
     <>
-      <mesh
-        scale={12}
-        position={[6, 4, -17]}
-        rotation={new THREE.Euler(-0.3, -0.5, 0)}
-        onClick={(e) => {
-          e.stopPropagation();
-          onSelect && onSelect();
-          document.documentElement.style.setProperty("cursor", "default");
-        }}
-        onPointerOver={() => selected === false && document.documentElement.style.setProperty("cursor", "pointer")}
-        onPointerLeave={() => document.documentElement.style.setProperty("cursor", "default")}
-      >
-        <primitive object={scene} ref={frameRef} />
-      </mesh>
-      <PortalBox selected={selected} />
+      <Float floatIntensity={selected ? 0 : 2} rotationIntensity={selected ? 0 : 0.1} speed={3}>
+        <mesh
+          scale={12}
+          position={[6, 4, -17]}
+          rotation={new THREE.Euler(-0.3, -0.5, 0)}
+          onClick={(e) => {
+            e.stopPropagation();
+            onSelect && onSelect();
+            document.documentElement.style.setProperty("cursor", "default");
+          }}
+          onPointerOver={() => selected === false && document.documentElement.style.setProperty("cursor", "pointer")}
+          onPointerLeave={() => document.documentElement.style.setProperty("cursor", "default")}
+        >
+          <primitive object={scene} ref={frameRef} />
+        </mesh>
+        <PortalBox selected={selected} />
+      </Float>
     </>
   );
 };
@@ -110,6 +112,23 @@ const PortalBox = ({ selected }: { selected: boolean }) => {
           <meshPhongMaterial color="#3A2D26" {...stencil} />
         </mesh>
       </group>
+
+      {/* <Html
+        as="div"
+        wrapperClass="w-[80%] max-w-[780px] h-[80vh] max-h-[506px] rounded-[4px] bg-ohi-front shadow-[4px_4px_12px_4px_rgba(0,_0,_0,_0.35)]"
+        // distanceFactor={10}
+        zIndexRange={[100, 0]}
+        // portal={domnodeRef}
+        transform
+        // sprite
+        // calculatePosition={(el: Object3D, camera: Camera, size: { width: number; height: number }) => number[]}
+        occlude="blending"
+        onOcclude={(visible) => null}
+        position={[6.9, 2.25, -21.5]}
+        rotation={new THREE.Euler(-0.3, -0.5, 0)}
+      >
+        <div className={``}></div>
+      </Html> */}
 
       {/* <spotLight position={[-11, 7, -16]} ref={spotRef} intensity={selected ? 300 : 0} color={"#ffffff"} angle={Math.PI / 4} distance={30} penumbra={0.1} castShadow /> */}
     </>
